5afb65c837003dbbf7e901de90bd688127861618
David Green <david.green@cd-adapco.com>
2021-02-02 11:39:27 -0500

    p001 solution

--- p001/timing.py
+++ p001/timing.py
@@ -1,4 +1,20 @@
 import timeit
-from p001.sum_of_multiples import multiples_of_any_range
+from p001.sum_of_multiples import multiples_up_to, mult_gen, m_merge
 
-print(timeit.timeit(lambda: multiples_of_any_range([3, 5], 10000000), number=1))
+limit = 10000000
+spacer = """
+"""
+
+
+def test(bases, limit):
+    print(timeit.timeit(lambda: sum(1 for _ in multiples_up_to(bases, limit)), number=1))
+    print(timeit.timeit(lambda: sum(1 for _ in mult_gen(bases, limit)), number=1))
+    print(timeit.timeit(lambda: sum(1 for _ in m_merge(bases, limit)), number=1))
+
+
+test([3, 5], limit)
+print(spacer)
+test([2, 3, 5, 7, 9, 11, 13, 15, 17], limit)
+print(spacer)
+test(range(2,100), limit)
+
